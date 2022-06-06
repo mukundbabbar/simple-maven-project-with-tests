f377f3627a2b90e62553696a4c72485d165ab708
mukundbabbar <mukundbabbar@gmail.com>
2022-06-06 13:42:18 +1000

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,6 +31,7 @@ class Base {
 
     protected void run() {
         double r = Math.random();
+        System.out.println("base");
         if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
